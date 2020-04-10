f6ac62a78c02be3e0e0d0ffee1749f6893aa58e3
Theophile Avoyne <tavoyne@gmail.com>
2020-04-10 16:00:19 +0200

    Finished MobilePreview

--- src/components/MobilePreview.js
+++ src/components/MobilePreview.js
@@ -1,23 +1,23 @@
-import React, {
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
+import PropTypes from 'prop-types';
+
+import React, { useContext, useEffect, useState } from 'react';
 
 import { Document, Page } from 'react-pdf/dist/entry.webpack';
 import styled from 'styled-components';
+import SVG from 'react-inlinesvg';
 
 import buildBlob from '../utils/buildBlob';
-import useHeight from '../hooks/useHeight';
+import useWidth from '../hooks/useWidth';
 
-import PageNavigation from './PageNavigation';
 import ContentContext from '../contexts/Content';
+import LoaderSVG from '../static/icons/loader.svg';
+import TimesSVG from '../static/icons/times.svg';
 
 const Button = styled.button`
   background: none;
   color: white;
   border: none;
+  display: flex;
   padding: 0;
   position: absolute;
   right: 10vw;
@@ -47,6 +47,32 @@ const DocumentContainer = styled.div`
   width: 80vw;
 `;
 
+const DownloadLink = styled.a`
+  background: ${({ theme }) => theme.halfBaked};
+  border-radius: 5px;
+  color: white;
+  font-weight: 500;
+  left: 50%;
+  padding: .5rem .75rem;
+  position: absolute;
+  text-decoration: none;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const LoaderIcon = styled(SVG)`
+  height: 3rem;
+  left: 50%;
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const TimesIcon = styled(SVG)`
+  color: white;
+  height: 1.5rem;
+`;
+
 const Wrapper = styled.div`
   background: ${({ theme }) => theme.sanJuan};
   bottom: 0;
@@ -60,19 +86,53 @@ const PreviewPanel = ({ close }) => {
   const [content] = useContext(ContentContext);
 
   const [blobURL, setBlobURL] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
 
-  const windowHeight = useHeight();
+  const windowWidth = useWidth();
+
+  useEffect(() => {
+    let didCancel = false;
+
+    const buildAndSetBlob = async () => {
+      const URL = await (buildBlob(content));
+      if (!didCancel) { setBlobURL(URL); }
+    };
+
+    buildAndSetBlob();
+
+    return () => {
+      didCancel = true;
+      if (blobURL) { URL.revokeObjectURL(blobURL); }
+    };
+  }, []);
 
   return (
     <Wrapper>
       <Controls>
-        <Button onClick={close}>Close</Button>
+        <DownloadLink download="Attestation.pdf" href={blobURL}>
+          Ouvrir
+        </DownloadLink>
+        <Button onClick={close}>
+          <TimesIcon src={TimesSVG} />
+        </Button>s
       </Controls>
-      <DocumentContainer />
+      <DocumentContainer>
+        <LoaderIcon src={LoaderSVG} />
+        {blobURL && (
+          <Document file={blobURL} loading={null}>
+            <Page
+              loading={null}
+              pageNumber={1}
+              width={windowWidth * 0.8}
+            />
+          </Document>
+        )}
+      </DocumentContainer>
     </Wrapper>
   );
+};
+
+PreviewPanel.propTypes = {
+  close: PropTypes.func.isRequired,
 };
 
 export default PreviewPanel;
